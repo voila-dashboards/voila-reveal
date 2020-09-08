8b100acc92f1f3bf30e19beec1c06f0e0b2d74db
Sylvain Corlay <sylvain.corlay@gmail.com>
2020-09-08 17:00:10 +0200

    Use voila 0.2.0b0

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup_args = {
     'packages': [],
     'data_files': data_files,
     'install_requires': [
-        'voila==0.2.0a3'
+        'voila==0.2.0b0'
     ],
     'author': 'Voila Development team',
     'author_email': 'jupyter@googlegroups.com',
